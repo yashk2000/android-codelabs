e0d9e9a299368cbcb4149efd312efa3c2132da96
yashk2000 <yashsja@gmail.com>
2019-09-11 18:28:40 +0530

    2.3

---
+++ 2.3/ImplicitIntentsReceiver/app/src/main/java/com/yashk2000/implicitintentsreceiver/MainActivity.java
@@ -0,0 +1,25 @@
+package com.yashk2000.implicitintentsreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        if (uri != null) {
+            String uri_string = "URI: " + uri.toString();
+            TextView textView = findViewById(R.id.text_uri_message);
+            textView.setText(uri_string);
+        }
+    }
+}
