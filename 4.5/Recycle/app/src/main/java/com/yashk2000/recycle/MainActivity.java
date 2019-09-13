98eaa359cd73bb0d165fbc1552bc662868ad490b
yashk2000 <yashsja@gmail.com>
2019-09-13 12:08:27 +0530

    4.5

---
+++ 4.5/Recycle/app/src/main/java/com/yashk2000/recycle/MainActivity.java
@@ -0,0 +1,72 @@
+package com.yashk2000.recycle;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + i);
+        }
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mAdapter = new WordListAdapter(this, mWordList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ 4.5/Recycle/app/src/main/java/com/yashk2000/recycle/WordListAdapter.java
@@ -0,0 +1,53 @@
+package com.yashk2000.recycle;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder>  {
+
+    private final LinkedList<String> mWordList;
+    private LayoutInflater mInflater;
+
+    public WordListAdapter(Context context, LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
+
+    @Override
+    public void onBindViewHolder(WordViewHolder holder, int position) {
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
+    }
+
+    @Override
+    public WordViewHolder onCreateViewHolder(ViewGroup parent,
+                                             int viewType) {
+        View mItemView = mInflater.inflate(R.layout.wordlist_item,
+                parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+
+    class WordViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+        public WordViewHolder(View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+        }
+    }
+}
