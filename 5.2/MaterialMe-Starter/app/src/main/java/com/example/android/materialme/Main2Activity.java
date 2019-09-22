281e40247b4a80aacdf11f11c4bceebe8d0b566b
yashk2000 <yashsja@gmail.com>
2019-09-22 14:53:48 +0530

    5.2

---
+++ 5.2/MaterialMe-Starter/app/src/main/java/com/example/android/materialme/Main2Activity.java
@@ -0,0 +1,38 @@
+package com.example.android.materialme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+/***
+ * Detail Activity that is launched when a list item is clicked.
+ * It shows more info on the sport.
+ */
+public class Main2Activity extends AppCompatActivity {
+
+    /**
+     * Initializes the activity, filling in the data from the Intent.
+     *
+     * @param savedInstanceState Contains information about the saved state
+     *                           of the activity.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        // Initialize the views.
+        TextView sportsTitle = findViewById(R.id.titleDetail);
+        ImageView sportsImage = findViewById(R.id.sportsImageDetail);
+
+        // Set the text from the Intent extra.
+        sportsTitle.setText(getIntent().getStringExtra("title"));
+
+        // Load the image using the Glide library and the Intent extra.
+        Glide.with(this).load(getIntent().getIntExtra("image_resource",0))
+                .into(sportsImage);
+    }
+}
